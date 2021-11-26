3f9fbaaa8333a6e66a37566348bb44da93780f82
zachratliff <zacharyratliff@g.harvard.edu>
2021-11-26 09:28:45 -0500

    More progress on protocol 3 and 4 of fast mental poker scheme

--- protocol.py
+++ protocol.py
@@ -1,6 +1,8 @@
 import secrets
 import hashlib
 
+SHUFFLE_SECURITY_PARAM = 64
+
 # Returns a non-interactive zero knowledge argument of knowledge
 # for discrete logarithm equality (over an Elliptic Curve sub group)
 # using the Fiat-Shamir Heuristic
@@ -39,7 +41,7 @@ def gen_rand_elem(curve):
     return (g, gx, h, hx, r, t)
 
 # Unbiased permutation generation
-# using Fisher-Yates shuffle
+# using Fisher-Yates shuffle.
 def fisher_yates_shuffle(s):
     for i in range(0, len(s) - 1):
         j = secrets.randbelow(len(s) - i)
@@ -50,3 +52,84 @@ def fisher_yates_shuffle(s):
         
     return s
 
+
+# Protocol 3 of Fast Mental Poker: Shuffle the Deck
+def shuffle_cards(deck):
+
+    shuffled_deck = deck
+
+    permutation = [i for i in range(1,len(deck))]
+    permutation = [0] + fisher_yates_shuffle(permutation)
+
+    for i in range(0, len(deck)):
+        x = secrets.randbelow(curve.field.n)
+        shuffled_deck[i] = deck[permutation[i]] * x
+
+    return (x, permutation, shuffled_deck)
+
+# Applies the specified permutation 
+# to the deck
+def apply_shuffle(deck, shuffle):
+    shuffled_deck = deck
+    for i in range(0, len(deck)):
+        shuffled_deck[i] = deck[shuffle[i]]
+
+
+# Takes in two permutations of equal length and
+# combines them into one e.g. \pi * \pi' 
+def compose_shuffles(s1, s2):
+    s = []
+    for idx in s2:
+        s.append(s1[idx])
+
+    return s
+
+# Protocol 4 of Fast Mental Poker: Shuffle Verification
+# We use Fiat-Shamir Heuristic to make the protocol
+# non-interactive.
+def gen_nizk_shuffle(deck):
+
+    # Use Protocol 3 to shuffle the deck
+    (x, p, deck) = shuffle_cards(deck)
+
+    # Generate NIZK proof
+    nizk = []
+    for i in range(0, SHUFFLE_SECURITY_PARAM):
+
+        # Shuffle the deck again
+        (y, p_prime, c) = shuffle_cards(deck) 
+
+        # Apply Fiat-Shamir Heuristic to non-interactively obtain a bit e
+        # We should probably be smarter about how we generate e
+        e = int(hashlib.sha256((str(c[i % len(deck)].x) + str(c[i % len(deck)].y)).encode('utf-8')).hexdigest(), 16) & 1
+
+        if e == 0:
+            nizk.append((c, y, p_prime))
+        else:
+            pp_prime = compose_shuffle(p, p_prime)
+            nizk.append((c, x * y, pp_prime))
+
+# Protocol 4 of Fast Mental Poker for NIZK Verification
+def verify_nizk_shuffle(deck, shuffled_deck, nizk):
+
+    for i in range(0, SHUFFLE_SECURITY_PARAM):
+        c = nizk[i][0]
+        y = nizk[i][1]
+        p = nizk[i][2] 
+
+        ds = deck
+        for j in range(0, len(deck)):
+            ds[j] *= y
+
+        ds = apply_shuffle(ds, p)
+
+        for j in range(0, len(deck)):
+            if ds[j] != c[j]:
+                return False
+
+    return True
+
+s1 = [0,4,5,3,2,1]
+s2 = [0,3,2,4,5,1]
+s3 = compose_shuffles(s1, s2)
+print(s3)
